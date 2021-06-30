b9fc5da560209a07695a0ab181c701e416b4aead
Martin Rios <mrios@froneus.com>
2021-06-30 12:30:35 -0300

    update

--- ml-project/app/api/routes/prediction.py
+++ ml-project/app/api/routes/prediction.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 import time
-<<<<<<< HEAD
-=======
-from loguru import logger
->>>>>>> 30f2a535ad77955183df747441782f5a9eb2a238
 
 from app.core import security
 from app.templates.input import InputTemplate
@@ -32,12 +28,8 @@ def post_predict(
     model: Model = request.app.state.model
     output: OutputTemplate = model.predict(input)
 
-<<<<<<< HEAD
 
     logger.debug(f"Campaña: '{input.campaign_name}' | id_survey_response: '{input.id_survey_response}' | PREDICTION TIME: {time.time() - start_time} seconds.")
     logger.debug(f"Campaña: '{input.campaign_name}' | id_survey_response: '{input.id_survey_response}' | SAVE: answer: {input.answer}, categ: {output.categ}, categ_proba: {output.categ_prob}")
-=======
-    logger.info("PREDICTION TIME: %s seconds." % (time.time() - start_time))
->>>>>>> 30f2a535ad77955183df747441782f5a9eb2a238
 
     return output
